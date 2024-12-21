3d6df0cbc473398b0077b484f980d7bb213e4116
Maddie <madel@example.com>
2024-12-21 18:35:53 -0500

    Creación de repositorio

---
+++ src/main/java/com/ups/medical/repositories/CitaRepository.java
@@ -0,0 +1,12 @@
+package com.ups.medical.repositories;
+
+import com.ups.medical.models.Cita;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Maddiekc
+ */
+
+public interface CitaRepository extends JpaRepository<Cita, Long>{
+}
